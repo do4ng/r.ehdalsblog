3e632fd819304c2f6d89cbf67fc1723784ec39c3
kangdongmandoo <kangdongmandoo@gmail.com>
2021-07-25 12:11:40 +0900

    Update config.js

--- post/config.js
+++ post/config.js
@@ -2,8 +2,15 @@ export let posts = [
   {
     title: "Hello, World!",
     url: "hello-world",
-    date: "2021/7/5",
+    date: "2021/7/23",
     category: "기타",
     thumbnail: "/static/images/img-hello-world.PNG",
+  },
+  {
+    title: "vitejs + svelte",
+    url: "vite-svelte",
+    date: "2021/7/24",
+    category: "svelte",
+    thumbnail: "/static/images/img-vite-js.png",
   },
 ];
