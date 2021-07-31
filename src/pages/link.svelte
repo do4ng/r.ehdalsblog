5dc1efcb74b9945305f31103334a04fbb3060c16
kangdongmandoo <kangdongmandoo@gmail.com>
2021-07-31 10:40:46 +0900

    Create link.svelte

---
+++ src/pages/link.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let params = { l: "" };
+  window.open(`#/p/${params.l}`, "_blank");
+</script>
+
+<div class="page-not-found">
+  <h2>🤖 페이지 이동 중..</h2>
+</div>
+
+<style>
+  .page-not-found {
+    margin-top: 90px;
+    text-align: center;
+  }
+</style>
