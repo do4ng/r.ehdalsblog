33258ba552fe041bfc9fdc57535f733e9db400bd
kangdongmandoo <kangdongmandoo@gmail.com>
2021-07-31 17:08:16 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   server: { fs: { allow: ["/service-worker.js"] } },
-  alias: {
-    "/@/": path.resolve("./src"),
-  },
 });
