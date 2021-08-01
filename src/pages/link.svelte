a3ecf5b719d10819410529eeee8bcd15a8d4eb53
kangdongmandoo <kangdongmandoo@gmail.com>
2021-08-01 10:56:59 +0900

    Update link.svelte

--- src/pages/link.svelte
+++ src/pages/link.svelte
@@ -1,6 +1,13 @@
 <script>
-  export let params = { l: "" };
-  window.open(`#/p/${params.l}`, "_blank");
+  export let params = { type: "", l: "" };
+
+  if (params.type === null || params.type === "post" || params.type === "p") {
+    history.pushState({}, "");
+    window.location.href = `#/p/${params.l}`;
+  }
+  if (params.type === "category" || params.type === "c") {
+    window.location.href = `#/c/${params.l}`;
+  }
 </script>
 
 <div class="page-not-found">
